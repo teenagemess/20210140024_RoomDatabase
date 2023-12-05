4bafefebc0262134d448f24f38668a893ce2939b
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 02:43:48 +0700

    menambah interface RepositoriSiswa

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.roomdatabase.repositori
+
+import com.example.roomdatabase.data.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun insertSiswa(siswa: Siswa)
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
+}
