a6c7a53f8344a2bdcc21c854c8517a355ee99a04
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 03:13:27 +0700

    membuat function untuk mengkonversi data input ke data dalam tabel sesuai jenis data nya

--- app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomdatabase.model
 
 import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.data.Siswa
 import com.example.roomdatabase.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){}
@@ -15,4 +16,24 @@ data class DetailSiswa(
     val nama: String = "",
     val alamat: String = "",
     val telpon: String = "",
+)
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
