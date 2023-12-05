e32156ceb6363b81f06973fe1fcd024af37449cb
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 03:39:39 +0700

    membuat class AplikasiSiswa yg akan digunakan oleh kelas kelas lainnya untuk mendapatkan depedensi

--- app/src/main/java/com/example/roomdatabase/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/AplikasiSiswa.kt
@@ -4,5 +4,14 @@ import android.app.Application
 import com.example.roomdatabase.repositori.ContainerApp
 import com.example.roomdatabase.repositori.ContainerDataApp
 class AplikasiSiswa : Application() {
+    /**
+     * Appcontainer instance digunakan oleh kelas kelas lainnya untuk mendapatkan depedensi
+     */
 
+    lateinit var container: ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
