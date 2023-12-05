2b63883d4e636301febacfbf60c9beb49b8c088d
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 03:15:20 +0700

    membuat beberapa function dalam class EntryViewModel untuk memanipulasi data

--- app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -1,10 +1,37 @@
 package com.example.roomdatabase.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.data.Siswa
 import com.example.roomdatabase.repositori.RepositoriSiswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){}
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    /*
+*Berisi status Siswa saat ini
+ */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa(){
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
