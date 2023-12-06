2011a458412dccd967d8454b37ba5b26a876300e
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 11:24:24 +0700

    membuat function SiswaTopAppBar untuk appbar

--- app/src/main/java/com/example/roomdatabase/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdatabase/ui/HalamanEntry.kt
@@ -4,25 +4,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.roomdatabase.R
 import com.example.roomdatabase.model.DetailSiswa
 import com.example.roomdatabase.model.UIStateSiswa
 import com.example.roomdatabase.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+
 
 @Composable
 fun EntrySiswaBody(
