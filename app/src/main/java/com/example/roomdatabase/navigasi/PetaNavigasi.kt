c6cf88f8a22bbc50f884ff1322799236fe48608c
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 11:43:09 +0700

    Menambah function HostNavigasi untuk navigasi route pada homescreen dan entrysiswascreen

--- app/src/main/java/com/example/roomdatabase/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/navigasi/PetaNavigasi.kt
@@ -11,7 +11,14 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.roomdatabase.R
+import com.example.roomdatabase.ui.DestinasiEntry
+import com.example.roomdatabase.ui.DestinasiHome
+import com.example.roomdatabase.ui.EntrySiswaScreen
+import com.example.roomdatabase.ui.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,4 +44,21 @@ fun SiswaTopAppBar(
             }
         }
     )
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
 }
