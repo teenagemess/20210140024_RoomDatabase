e02adebe556de28244b788a63e095e633768194f
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-06 11:32:03 +0700

    Membuat halaman home dan membuat object DestinasiHome beserta variable nya untuk menentukan route destinasi

---
+++ app/src/main/java/com/example/roomdatabase/ui/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.ui
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
